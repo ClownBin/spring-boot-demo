fdcd772c141448807c61e19e8c1fdd1c5678f601
ClownBin <chaobin1993@126.com>
2019-04-12 14:28:55 +0800

    Create RoleCheck.java

---
+++ src/main/java/com/example/demo/annotation/aop/RoleCheck.java
@@ -0,0 +1,26 @@
+package com.example.demo.annotation.aop;
+
+import java.lang.annotation.*;
+
+/**
+ * Check cluster role.
+ * Example:
+ * \@RoleCheck(#creatorId)
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ClusterRoleCheck {
+
+    /**
+     * 0. 集群管理员， 1.集群普通用户
+     * 传入用户id
+     */
+    String creatorId() default "";
+
+    /**
+     * 集群id
+     */
+    String clusterId() default "";
+
+}
